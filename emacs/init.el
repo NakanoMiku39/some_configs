7a582a51fcc85fda2182a39804583c7218d43c57
NakanoMiku <jinleho@hotmail.com>
2026-01-20 01:24:55 +0800

    Update: Emacs

--- emacs/init.el
+++ emacs/init.el
@@ -220,8 +220,7 @@
 
 ;; 记录undo历史
 (use-package undo-tree
-  :ensure t
-  :defer 2
+  :ensure t  
   :init (global-undo-tree-mode)
   :custom
   (undo-tree-auto-save-history nil))
@@ -327,6 +326,9 @@
     ;; 优化性能
     (setq eglot-events-buffer-size 0) ;; 关闭 event log 以提升性能
     (setq eglot-autoshutdown t)       ;; 关闭最后一个 buffer 时关闭 server
+    ;; 禁止 Inlay Hints 和 请求语义高亮
+    (setq eglot-ignored-server-capabilities '(:inlayHintProvider
+					      :semanticTokensProvider))
 
     ;; 配置 C/C++ 使用特定的 clangd 版本
     (add-to-list 'eglot-server-programs '((c-mode c++-mode) "clangd-19"))
@@ -396,6 +398,32 @@
    minibuffer-local-completion-map)
    (setq sbt:program-options '("-Dsbt.supershell=false")))
 
+ (use-package projectile
+  :straight t
+  :ensure t
+  :bind (("C-c p" . projectile-command-map))
+  :config
+  (setq projectile-mode-line "Projectile")
+  (setq projectile-track-known-projects-automatically nil))
+
+(use-package counsel-projectile
+  :straight t
+  :ensure t
+  :after (projectile)
+  :init (counsel-projectile-mode))
+
+(use-package dashboard
+  :straight t
+  :ensure t
+  :config
+  (setq dashboard-banner-logo-title "Welcome to Emacs!") ;; 个性签名，随读者喜好设置
+  (setq dashboard-projects-backend 'projectile) ;; 读者可以暂时注释掉这一行，等安装了 projectile 后再使用
+  (setq dashboard-startup-banner 'official) ;; 也可以自定义图片
+  (setq dashboard-items '((recents  . 10)   ;; 显示多少个最近文件
+			  (bookmarks . 5)  ;; 显示多少个最近书签
+			  (projects . 10))) ;; 显示多少个最近项目
+  (dashboard-setup-startup-hook))
+
 ;; ============================================================
 ;; Treemacs (通用文件树，不依赖具体 LSP)
 ;; ============================================================
@@ -424,6 +452,7 @@
 (use-package lsp-treemacs
   :ensure t
   :after (treemacs lsp))
+
 
 (provide 'init)
 

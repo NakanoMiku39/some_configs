9ed40eba5ef54e680f88ff4f105e01c8543f2b35
NakanoMiku <jinleho@hotmail.com>
2026-01-20 15:17:31 +0800

    Update: Emacs
    Use doom-dracula theme

--- .emacs.d/lisp/theme.el
+++ .emacs.d/lisp/theme.el
@@ -4,6 +4,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold nil    ; if nil, bold is universally disabled
 	doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-monokai-octagon t)
-  (doom-themes-treemacs-config))
+  (load-theme 'doom-dracula t)
+  (doom-themes-treemacs-config)
+  (doom-themes-visual-bell-config))
 (provide 'theme)
